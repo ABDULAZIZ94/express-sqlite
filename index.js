b6c330a05f513e79580df391798f8a15a41718f8
ABDULAZIZ94 <abdulazizabdulnoor@gmail.com>
2020-05-30 09:38:17 +0800

    moves all db experiments to sqlite_disk.js

--- index.js
+++ index.js
@@ -12,24 +12,6 @@ let db = new sqlite3.Database("./db/chinook.db", (err) => {
   console.log("Connected to the chinook database.");
 });
 
-db.run("CREATE TABLE IF NOT EXISTS langs(name text)");
-
-let languages = ["C++", "Python", "Java", "C#", "Go"];
-
-// construct the insert statement with multiple placeholders
-// based on the number of rows
-let placeholders = languages.map((language) => "(?)").join(",");
-let sql = "INSERT INTO langs(name) VALUES " + placeholders;
-
-// insert one row into the langs table
-db.run(sql, placeholders, function (err) {
-  if (err) {
-    return console.log(err.message);
-  }
-  // get the last insert id
-  console.log(`A row has been inserted with rowid ${this.lastID}`);
-});
-
 // close the database connection
 db.close((err) => {
   if (err) {
--- sqlite_disk.js
+++ sqlite_disk.js
@@ -7,6 +7,37 @@ let db = new sqlite3.Database("./db/chinook.db", (err) => {
   console.log("Connected to the chinook database.");
 });
 
+db.run("CREATE TABLE IF NOT EXISTS langs(name text)");
+
+let languages = ["C++", "Python", "Java", "C#", "Go"];
+
+// construct the insert statement with multiple placeholders
+// based on the number of rows
+let placeholders = languages.map((language) => "(?)").join(",");
+
+let sql = "INSERT INTO langs(name) VALUES " + placeholders;
+
+// insert one row into the langs table
+db.run(sql, placeholders, function (err) {
+  if (err) {
+    return console.log(err.message);
+  }
+  // get the last insert id
+  console.log(`A row has been inserted with rowid ${this.lastID}`);
+});
+
+let sql2 = `SELECT DISTINCT name FROM langs
+           ORDER BY name`;
+
+db.all(sql2, [], (err, rows) => {
+  if (err) {
+    throw err;
+  }
+  rows.forEach((row) => {
+    console.log(row.name);
+  });
+});
+
 // close the database connection
 db.close((err) => {
   if (err) {
