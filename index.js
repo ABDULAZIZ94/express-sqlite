e738079c45ce16b45e5503fabbb44a6804e13e00
ABDULAZIZ94 <abdulazizabdulnoor@gmail.com>
2020-05-30 08:58:06 +0800

    connecting to cinebook database ok

--- index.js
+++ index.js
@@ -2,30 +2,22 @@ const express = require("express");
 const app = express();
 const port = 80;
 const hostname = "0.0.0.0";
+const sqlite3 = require("sqlite3").verbose();
 
-const Database = require("sqlite3").verbose;
-
-var db = new Database("db/hadith", (err) => {
+// open database in memory
+let db = new sqlite3.Database("./db/chinook.db", (err) => {
   if (err) {
     console.error(err.message);
-  } else {
-    console.log("Connected to database.");
   }
+  console.log("Connected to the chinook database.");
 });
-
-db.run("CREATE TABLE lorem (info TEXT)");
-
-var stmt = db.prepare("INSERT INTO lorem VALUES (?)");
-for (var i = 0; i < 10; i++) {
-  stmt.run("Ipsum " + i);
-}
-stmt.finalize();
-
-db.each("SELECT rowid AS id, info FROM lorem", function (err, row) {
-  console.log(row.id + ": " + row.info);
+// close the database connection
+db.close((err) => {
+  if (err) {
+    return console.error(err.message);
+  }
+  console.log("Close the database connection.");
 });
-
-db.close();
 
 app.get("/", (req, res) => {
   res.send("ok");
