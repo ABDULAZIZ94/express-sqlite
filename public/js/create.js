0c3aa3a424741177ff3d8668209852dfb82acfd4
ABDULAZIZ94 <abdulazizabdulnoor@gmail.com>
2020-06-02 07:20:53 +0800

    add create.js & implement on create.pug

---
+++ public/js/create.js
@@ -0,0 +1,17 @@
+//general function
+function post_any_data (url, [...form]){
+  $.ajax({
+    method: "POST",
+    url: url,
+    contentType:'application/x-www-form-urlencoded; charset=UTF-8',
+    data: form//{ table_name: "HADITH" } //use data not body
+  })
+}
+//specific function
+function post_form2(){
+  var form_data = $('#form2').serializeArray();
+  // console.log(form_data);
+  post_any_data('/create/insertrow', form_data);
+}
+
+
